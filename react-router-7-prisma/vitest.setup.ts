63fe18a5833154f4db2f32ce0dc07869a4adfc00
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-09-28 09:10:25 +0900

    テスト全実行時のエラー対処

--- react-router-7-prisma/vitest.setup.ts
+++ react-router-7-prisma/vitest.setup.ts
@@ -1,1 +1,8 @@
-import "@testing-library/jest-dom/vitest"
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+// Reset the DOM and unmount components after each test
+afterEach(() => {
+  cleanup();
+});
