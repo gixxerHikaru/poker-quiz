4b648a7530fd9b90fd5e752aa9ed12d14076ed0c
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-11-30 23:40:50 +0900

    変数名をresultに変更

--- react-router-7-prisma/work/main.ts
+++ react-router-7-prisma/work/main.ts
@@ -23,12 +23,12 @@ export function statement(invoice: any, plays: any) {
   return result;
 
   function volumeCreditsFor(perf: any) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
 
     if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits;
+      result += Math.floor(perf.audience / 5);
+    return result;
   }
 
   function playFor(aPerformance: any) {
