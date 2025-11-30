b4378d066fbfbd4db31c14a64fb5d0125d65489a
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-11-30 23:24:17 +0900

    関数宣言の変更

--- react-router-7-prisma/work/main.ts
+++ react-router-7-prisma/work/main.ts
@@ -31,27 +31,26 @@ export function statement(invoice: any, plays: any) {
   function playFor(aPerformance: any) {
     return plays[aPerformance.playID];
   }
-}
+  function amountFor(play: any, aPerformance: any) {
+    let result = 0;
 
-function amountFor(play: any, aPerformance: any) {
-  let result = 0;
-
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
   }
-  return result;
 }
