54529fac86cbc6e74fff60c906374f29b80b0027
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-11-30 23:04:49 +0900

    リファクタリング初期状態作成

---
+++ react-router-7-prisma/work/main.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { statement } from "./main";
+import invoice from "./invoices.json";
+import plays from "./plays.json";
+
+describe("statement", () => {
+  it("should generate correct statement", () => {
+    const result = statement(invoice[0], plays);
+
+    const expected = [
+      "Statement for BigCo",
+      " Hamlet: $650.00 (55 seats)",
+      " As You Like It: $580.00 (35 seats)",
+      " Othello: $500.00 (40 seats)",
+      "Amount owed is $1,730.00",
+      "You earned 47 credits",
+      "",
+    ].join("\n");
+
+    expect(result).toBe(expected);
+  });
+});
