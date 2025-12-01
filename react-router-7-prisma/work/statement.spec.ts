e0676ac2cc1d74f859b70f031e21964001f880ac
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-02 08:10:19 +0900

    テストの修正

--- react-router-7-prisma/work/statement.spec.ts
+++ react-router-7-prisma/work/statement.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { statement } from "./main";
+import { statement } from "./statement";
 import invoice from "./invoices.json";
 import plays from "./plays.json";
 
