260ab942405f9afe43f43de53a91cbf7d0f3762e
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-01 21:49:43 +0900

    変数名をresultに変更

--- react-router-7-prisma/work/main.ts
+++ react-router-7-prisma/work/main.ts
@@ -10,11 +10,11 @@ export function statement(invoice: any, plays: any) {
   return result;
 
   function totalAmount() {
-    let totalAmount = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf);
+      result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
   }
 
   function totalVolumeCredits() {
