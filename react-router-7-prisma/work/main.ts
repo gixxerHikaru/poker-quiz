25bfefea43a1261fd0d72581d825efd3740f7fcb
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-01 21:43:43 +0900

    ループの分離・不要なコメント削除

--- react-router-7-prisma/work/main.ts
+++ react-router-7-prisma/work/main.ts
@@ -3,8 +3,10 @@ export function statement(invoice: any, plays: any) {
 
   let totalAmount = 0;
   for (let perf of invoice.performances) {
-    // 注文の内訳を出力
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+  }
+
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
