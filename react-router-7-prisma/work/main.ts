5225a0314a4a14c73cf990646a39e59c9583b89b
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-02 08:05:43 +0900

    html版の追加

--- react-router-7-prisma/work/main.ts
+++ react-router-7-prisma/work/main.ts
@@ -23,3 +23,29 @@ function renderPlainText(data: any) {
     }).format(aNumber / 100);
   }
 }
+
+function htmlStatement(invoice: any, plays: any) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data: any) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>Play</th><th>Seats</th><th>Cost</th></tr>`;
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>`;
+  }
+  result += `</table>`;
+  result += `<p>Amount owed is ${usd(data.totalAmount)}</p>\n`;
+  result += `<p>You earned ${data.totalVolumeCredits} credits</p>\n`;
+
+  return result;
+
+  function usd(aNumber: number) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+}
