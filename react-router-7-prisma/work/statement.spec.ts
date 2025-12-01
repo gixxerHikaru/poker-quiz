fd218b2b626a9af7ece7fc402ecb85ae1378ff1b
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-02 07:55:02 +0900

    ファイル名の変更

--- react-router-7-prisma/work/statement.spec.ts
+++ react-router-7-prisma/work/statement.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { htmlStatement } from "./main";
+import { htmlStatement } from "./statement";
 import invoice from "./invoices.json";
 import plays from "./plays.json";
 
