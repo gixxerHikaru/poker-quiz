697206e2ef7ac4e9da394b288504d826477134ec
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-01 23:10:39 +0900

    関数のファイル（とフェーズ）抽出

--- react-router-7-prisma/work/main.ts
+++ react-router-7-prisma/work/main.ts
@@ -1,73 +1,7 @@
+import { createStatementData } from "./createStatementData";
+
 export function statement(invoice: any, plays: any) {
   return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice: any, plays: any) {
-  const statementData: any = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
-
-  function enrichPerformance(aPerformance: any) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-
-    return result;
-  }
-  function playFor(aPerformance: any) {
-    return plays[aPerformance.playID];
-  }
-}
-
-function amountFor(aPerformance: any) {
-  let result = 0;
-
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`);
-  }
-  return result;
-}
-function volumeCreditsFor(aPerformance: any) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  if ("comedy" === aPerformance.play.type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function totalAmount(data: any) {
-  let result = 0;
-  for (let perf of data.performances) {
-    result += perf.amount;
-  }
-  return result;
-}
-
-function totalVolumeCredits(data: any) {
-  let result = 0;
-  for (let perf of data.performances) {
-    result += perf.volumeCredits;
-  }
-  return result;
 }
 
 function renderPlainText(data: any) {
