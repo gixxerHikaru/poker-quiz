ed2be73863565253f5db99cda9bc082b32e9ded7
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-06 18:44:03 +0900

    不足していたテストの追加とそれに合わせて実装の修正

--- react-router-7-prisma/work/statement.spec.ts
+++ react-router-7-prisma/work/statement.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { statement } from "./statement";
+import { statement, htmlStatement } from "./statement";
 import invoice from "./invoices.json";
 import plays from "./plays.json";
 
@@ -14,6 +14,27 @@ describe("statement", () => {
       " Othello: $500.00 (40 seats)",
       "Amount owed is $1,730.00",
       "You earned 47 credits",
+      "",
+    ].join("\n");
+
+    expect(result).toBe(expected);
+  });
+});
+
+describe("htmlStatement", () => {
+  it("should generate correct statement", () => {
+    const result = htmlStatement(invoice[0], plays);
+
+    const expected = [
+      "<h1>Statement for BigCo</h1>",
+      "<table>",
+      "<tr><th>Play</th><th>Seats</th><th>Cost</th></tr>",
+      " <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>",
+      " <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>",
+      " <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>",
+      "</table>",
+      "<p>Amount owed is <em>$1,730.00</em></p>",
+      "<p>You earned <em>47</em> credits</p>",
       "",
     ].join("\n");
 
--- react-router-7-prisma/work/statement.ts
+++ react-router-7-prisma/work/statement.ts
@@ -16,20 +16,20 @@ function renderPlainText(data: any) {
   return result;
 }
 
-function htmlStatement(invoice: any, plays: any) {
+export function htmlStatement(invoice: any, plays: any) {
   return renderHtml(createStatementData(invoice, plays));
 }
 
 function renderHtml(data: any) {
   let result = `<h1>Statement for ${data.customer}</h1>\n`;
   result += `<table>\n`;
-  result += `<tr><th>Play</th><th>Seats</th><th>Cost</th></tr>`;
+  result += `<tr><th>Play</th><th>Seats</th><th>Cost</th></tr>\n`;
   for (let perf of data.performances) {
-    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>`;
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>\n`;
   }
-  result += `</table>`;
-  result += `<p>Amount owed is ${usd(data.totalAmount)}</p>\n`;
-  result += `<p>You earned ${data.totalVolumeCredits} credits</p>\n`;
+  result += `</table>\n`;
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
 
   return result;
 }
