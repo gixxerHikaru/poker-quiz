a8ae97f9cd2ee52887aab9c62a8c58b3174d6283
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-12-26 20:49:16 +0900

    prettierrcの配備

--- react-router-7-prisma/vitest.config.ts
+++ react-router-7-prisma/vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
 
-    setupFiles: ["./vitest.setup.ts"],
+    setupFiles: ['./vitest.setup.ts'],
   },
 });
